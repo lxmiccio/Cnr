38499c79f19519b99d1f9474166d30f2288b0659
lxmiccio <miccio.alex@gmail.com>
2016-01-03 13:46:12 +0100

    Update Personale.php

--- Personale.php
+++ Personale.php
@@ -1,89 +1,47 @@
 <?php
-    class Year {
-        public $location;
-        public $number;
-        public $year;
-
-        public function __construct($location, $year) {
-            $this->location = $location;
-            $this->number = 1;
-            $this->year = $year;
-        }
-
-        public function increaseNumber() {
-            $this->number++;
-        }
-    }
-
     require("CredenzialiPersonale.php");
     if (isset($_REQUEST['lat'])) {
-        $latitude = $_REQUEST['lat'];
+        $latitude1 = $_REQUEST['lat'];
     } else {
         die("Fatal Error:    Cannot Get Latitude");
     }
     if (isset($_REQUEST['lng'])) {
-        $longitude = $_REQUEST['lng'];
+        $longitude1 = $_REQUEST['lng'];
     } else {
         die("Fatal Error:    Cannot Get Longitude");
     }
     if (isset($_REQUEST['lat1'])) {
-        $latitude1 = $_REQUEST['lat1'];
+        $latitude2 = $_REQUEST['lat1'];
     } else {
         die("Fatal Error:    Cannot Get Latitude1");
     }
     if (isset($_REQUEST['lng1'])) {
-        $longitude1 = $_REQUEST['lng1'];
+        $longitude2 = $_REQUEST['lng1'];
     }
     else {
         die("Fatal Error:    Cannot Get Longitude1");
     }
-    if ($latitude == 16.21915709999996 && $longitude == 39.3277825) {
-        $sede = "Cosenza";
-    } else if ($latitude == 16.88221120000003 && $longitude == 41.1122236) {
-        $sede = "Bari";
-    } else if ($latitude == 12.363586127758026 && $longitude == 43.09344450421002) {
-        $sede = "Perugia";
-    } else if ($latitude == 7.638289999999984 && $longitude == 45.0162) {
-        $sede = "Torino";
-    } else if ($latitude == 11.92901 && $longitude == 45.39468) {
-        $sede = "Padova";
-    }
-    if ($latitude1 == 16.21915709999996 && $longitude1 == 39.3277825) {
-        $sede1 = "Cosenza";
-    } else if ($latitude1 == 16.88221120000003 && $longitude1 == 41.1122236) {
-        $sede1 = "Bari";
-    } else if ($latitude1 == 12.363586127758026 && $longitude1 == 43.09344450421002) {
-        $sede1 = "Perugia";
-    } else if ($latitude1 == 7.638289999999984 && $longitude1 == 45.0162) {
-        $sede1 = "Torino";
-    } else if ($latitude1 == 11.92901 && $longitude1 == 45.39468) {
-        $sede1 = "Padova";
-    }
-    $csv   = "date,Laureati,Sede" . "\n";
-    $years = array();
-    $connection = pg_connect("dbname=" . $db_name . " host=" . $db_host . " password=" . $db_psw . " port=" . $db_port . " user=" . $db_user) or die("Fatal Error:    Cannot connect to " . $db_name);
-    $query = 'SELECT "data_laurea1", "SEZIONE" FROM "Personale_IRPI_2" WHERE "SEZIONE"=\'' . $sede . '\' OR "SEZIONE"=\'' . $sede1 . '\'';
-    $result = pg_query($connection, $query) or die("Fatal Error:    Cannot Perform Query");
-    while ($row = pg_fetch_row($result)) {
-        if (!count($years)) {
-            array_push($years, new Year($row[1], date("Y", strtotime($row[0]))));
-        } else {
-            $found = false;
-            foreach ($years as $year) {
-                if (strlen($row[0]) !== 0 && strlen($row[0]) !== 0) {
-                    if (!strcmp($year->location, $row[1]) && $year->year === date("Y", strtotime($row[0]))) {
-                        $found = true;
-                        $year->increaseNumber();
-                    }
-                }
-            }
-            if (!$found && strlen($row[0]) !== 0) {
-                array_push($years, new Year($row[1], date("Y", strtotime($row[0]))));
-            }
+
+    $geojson = json_decode(file_get_contents("cnr.geojson"), true);
+    for($iterator = 0; $iterator < count($geojson['features']); $iterator++) {
+        if($latitude1 == $geojson["features"][$iterator]["geometry"]["coordinates"][0] && $longitude1 == $geojson["features"][$iterator]["geometry"]["coordinates"][1]) {
+            $sede1 = $geojson["features"][$iterator]["properties"]["City"];
+        }
+        if($latitude2 == $geojson["features"][$iterator]["geometry"]["coordinates"][0] && $longitude2 == $geojson["features"][$iterator]["geometry"]["coordinates"][1]) {
+            $sede2 = $geojson["features"][$iterator]["properties"]["City"];
         }
     }
-    foreach ($years as $year) {
-        $csv .= $year->year . "," . $year->number . "," . $year->location . "\n";
+
+    $csv   = "date,Laureati,Sede" . "\n";
+    $connection = pg_connect("dbname=" . $db_name . " host=" . $db_host . " password=" . $db_psw . " port=" . $db_port . " user=" . $db_user) or die("Fatal Error:    Cannot connect to " . $db_name);
+    $query = "SELECT replace(\"data_laurea1\", '/', '-'), COUNT(*), \"SEZIONE\" 
+                FROM \"Personale_IRPI_2\" 
+                WHERE \"data_laurea1\" IS NOT NULL AND \"SEZIONE\" IS NOT NULL AND (\"SEZIONE\"='" . $sede1 . "' OR \"SEZIONE\"='" . $sede2 . "') 
+                GROUP BY \"data_laurea1\", \"SEZIONE\" 
+                ORDER BY \"SEZIONE\", to_timestamp(replace(\"data_laurea1\", '/', '-'), 'DD-MM-YYYY hh24:mi:ss')::timestamp without time zone";
+    $result = pg_query($connection, $query) or die("Fatal Error:    Cannot Perform Query");
+    while ($row = pg_fetch_row($result)) {
+        $csv .= $row[0] . "," . $row[1] . "," . $row[2] . "\n";
     }
     echo $csv;
 ?>
